3497f6f7fc927ba0a63d7982890b848bc8b35a75
Kyle Stachowicz <kylestach99@gmail.com>
2017-01-09 13:25:46 -0800

    Fix initialization sequence

--- src/wpilib/robot.rs
+++ src/wpilib/robot.rs
@@ -1,25 +1,20 @@
-use wpilib::wpilib_hal;
+use wpilib::wpilib_hal::*;
+use std::ffi;
 
 pub trait Robot: Sized {
     fn run(self);
 
     fn new() -> Self;
-}
 
-#[macro_export]
-macro_rules! start_robot_class {
-    ($robot_class:ident) => {
-        fn main() {
-            unsafe {
-                let status = wpilib_hal::HAL_Initialize(0);
-                if status != 0 {
-                    panic!("WPILib HAL failed to initialize with error {}", status);
-                }
-                // Report the language as something other than the supported ones
-                wpilib_hal::HAL_Report(2, 6, 0, std::ptr::null());
-                let robot = $robot_class::new();
-                robot.run();
+    fn main() {
+        // Initialize HAL
+        unsafe {
+            let status = HAL_Initialize(0);
+            if status != 1 {
+                panic!("WPILib HAL failed to initialize!");
             }
         }
+        let robot = Self::new();
+        robot.run();
     }
 }
