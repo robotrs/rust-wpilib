4fb651a664b8e287c176e304369cfb5307b337ac
Kyle Stachowicz <kylestach99@gmail.com>
2017-01-11 17:31:28 -0800

    Fix robot state functions to return correctly

--- src/wpilib/robot_state.rs
+++ src/wpilib/robot_state.rs
@@ -1,11 +1,12 @@
 use wpilib::wpilib_hal::*;
+use wpilib::hal_call::*;
 
 pub fn is_browned_out() -> HalResult<bool> {
-    Ok(hal_call!(HAL_GetBrownedOut())? as bool)
+    Ok(hal_call!(HAL_GetBrownedOut())? != 0)
 }
 
 pub fn is_system_active() -> HalResult<bool> {
-    Ok(hal_call!(HAL_GetSystemActive())? as bool)
+    Ok(hal_call!(HAL_GetSystemActive())? != 0)
 }
 
 pub fn get_battery_voltage() -> HalResult<f64> {
