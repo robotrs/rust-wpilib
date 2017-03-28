ba008c8544c2dbf78bcfe335a18cbefbf78ea176
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-03-28 12:28:24 -0400

    Code cleanup

--- src/wpilib/joystick.rs
+++ src/wpilib/joystick.rs
@@ -7,8 +7,8 @@ const RUMBLE_BASE: i32 = 65535;
 
 #[derive(PartialEq)]
 pub enum JoystickSide {
-    kLeftHand,
-    kRightHand,
+    LeftHand,
+    RightHand,
 }
 
 #[derive(PartialEq)]
@@ -51,21 +51,21 @@ impl JoystickBase for Joystick {
     fn get_raw_axis(&mut self, axis: usize) -> f32 {
         match self.ds.get_joystick_axis(self.port, axis) {
             Ok(val) => val,
-            Err(e) => 0f32,
+            _ => 0f32,
         }
     }
 
     fn get_raw_button(&mut self, button: usize) -> bool {
         match self.ds.get_joystick_button(self.port, button) {
             Ok(val) => val,
-            Err(e) => false,
+            _ => false,
         }
     }
 
     fn get_pov(&mut self, pov: usize) -> i16 {
         match self.ds.get_joystick_pov(self.port, pov) {
             Ok(val) => val,
-            Err(e) => -01,
+            _ => -01,
         }
     }
 
@@ -81,17 +81,12 @@ impl JoystickBase for Joystick {
     }
 
     fn set_rumble(&mut self, side: JoystickSide, mut value: f32) {
-        if value < 0f32 {
-            value = 0f32;
-        } else if value > 1f32 {
-            value = 1f32;
+        value = if value > 1f32 { 1f32 } else if value < 0f32 { 0f32 } else { value };
+        match side {
+            JoystickSide::LeftHand => self.left_rumble = (value * RUMBLE_BASE as f32) as i32,
+            JoystickSide::RightHand => self.right_rumble = (value * RUMBLE_BASE as f32) as i32,
         }
-        if side == JoystickSide::kLeftHand {
-            self.left_rumble = (value * RUMBLE_BASE as f32) as i32;
-        } else {
-            self.right_rumble = (value * RUMBLE_BASE as f32) as i32;
-        }
-        unsafe { HAL_SetJoystickOutputs(self.port as i32, self.outputs, self.left_rumble, self.right_rumble); }
+        unsafe { HAL_SetJoystickOutputs(self.port as i32, self.outputs, self.left_rumble, self.right_rumble) };
     }
 }
 
@@ -126,10 +121,9 @@ impl XBoxController {
     }
 
     pub fn get_bumper(&mut self, side: JoystickSide) -> bool {
-        if side == JoystickSide::kLeftHand {
-            self.get_raw_button(5)
-        } else {
-            self.get_raw_button(6)
+        match side {
+            JoystickSide::LeftHand => self.get_raw_button(5),
+            JoystickSide::RightHand => self.get_raw_button(6),
         }
     }
 
@@ -142,35 +136,31 @@ impl XBoxController {
     }
 
     pub fn get_axis_button(&mut self, side: JoystickSide) -> bool {
-        if side == JoystickSide::kLeftHand {
-            self.get_raw_button(9)
-        } else {
-            self.get_raw_button(10)
+        match side {
+            JoystickSide::LeftHand => self.get_raw_button(9),
+            JoystickSide::RightHand => self.get_raw_button(10),
         }
     }
 
     //axes
     pub fn get_x(&mut self, side: JoystickSide) -> f32 {
-        if side == JoystickSide::kLeftHand {
-            self.get_raw_axis(0)
-        } else {
-            self.get_raw_axis(4)
+        match side {
+            JoystickSide::LeftHand => self.get_raw_axis(0),
+            JoystickSide::RightHand => self.get_raw_axis(4),
         }
     }
 
     pub fn get_y(&mut self, side: JoystickSide) -> f32 {
-        if side == JoystickSide::kLeftHand {
-            self.get_raw_axis(1)
-        } else {
-            self.get_raw_axis(5)
+        match side {
+            JoystickSide::LeftHand => self.get_raw_axis(1),
+            JoystickSide::RightHand => self.get_raw_axis(5),
         }
     }
 
     pub fn get_trigger(&mut self, side: JoystickSide) -> f32 {
-        if side == JoystickSide::kLeftHand {
-            self.get_raw_axis(2)
-        } else {
-            self.get_raw_axis(3)
+        match side {
+            JoystickSide::LeftHand => self.get_raw_axis(2),
+            JoystickSide::RightHand => self.get_raw_axis(3),
         }
     }
 
@@ -193,21 +183,21 @@ impl JoystickBase for XBoxController {
     fn get_raw_axis(&mut self, axis: usize) -> f32 {
         match self.ds.get_joystick_axis(self.port, axis) {
             Ok(val) => val,
-            Err(e) => 0f32,
+            _ => 0f32,
         }
     }
 
     fn get_raw_button(&mut self, button: usize) -> bool {
         match self.ds.get_joystick_button(self.port, button) {
             Ok(val) => val,
-            Err(e) => false,
+            _ => false,
         }
     }
 
     fn get_pov(&mut self, pov: usize) -> i16 {
         match self.ds.get_joystick_pov(self.port, pov) {
             Ok(val) => val,
-            Err(e) => -01,
+            _ => -01,
         }
     }
 
@@ -223,15 +213,10 @@ impl JoystickBase for XBoxController {
     }
 
     fn set_rumble(&mut self, side: JoystickSide, mut value: f32) {
-        if value < 0f32 {
-            value = 0f32;
-        } else if value > 1f32 {
-            value = 1f32;
-        }
-        if side == JoystickSide::kLeftHand {
-            self.left_rumble = (value * RUMBLE_BASE as f32) as i32;
-        } else {
-            self.right_rumble = (value * RUMBLE_BASE as f32) as i32;
+        value = if value > 1f32 { 1f32 } else if value < 0f32 { 0f32 } else { value };
+        match side {
+            JoystickSide::LeftHand => self.left_rumble = (value * RUMBLE_BASE as f32) as i32,
+            JoystickSide::RightHand => self.right_rumble = (value * RUMBLE_BASE as f32) as i32,
         }
         unsafe { HAL_SetJoystickOutputs(self.port as i32, self.outputs, self.left_rumble, self.right_rumble) };
     }
