b7474d4f77ee432a807e30b7f0cacc0f646e7a24
Kyle Stachowicz <kylestach99@gmail.com>
2017-03-31 11:40:14 -0700

    Add BufferedPcm for buffered CAN output

--- src/wpilib/pcm.rs
+++ src/wpilib/pcm.rs
@@ -80,3 +80,63 @@ impl Drop for Solenoid {
         }
     }
 }
+
+/// A struct representing a PCM object that is buffered and stores values until it is flushed. This
+/// object can create BufferedSolenoids to act as proxies for setting PCM values.
+pub struct BufferedPcm {
+    buffer: sync::Mutex<u8>,
+    module: i32,
+}
+
+/// A single-acting solenoid that caches out values to a BufferedPcm instead of writing directly to
+/// CAN.
+pub struct BufferedSolenoid<'a> {
+    buffer: &'a sync::Mutex<u8>,
+    channel: i32,
+}
+
+impl BufferedPcm {
+    /// Create a new BufferedPcm on the specified module, returning None if the module is invalid.
+    pub fn new(module: i32) -> Result<BufferedPcm, SolenoidCreationError> {
+        if !sensor::check_solenoid_module(module) {
+            Err(SolenoidCreationError::ModuleDNE)
+        } else {
+            Ok(BufferedPcm {
+                buffer: sync::Mutex::new(0),
+                module: module,
+            })
+        }
+    }
+
+    /// Make a new BufferedSolenoid on this PCM on the specified channel, returning None if the
+    /// channel is invalid.
+    pub fn make_solenoid<'a>(&'a self, channel: i32) -> Result<BufferedSolenoid<'a>, SolenoidCreationError> {
+        if !sensor::check_solenoid_channel(channel) {
+            Err(SolenoidCreationError::ChannelDNE)
+        } else {
+            Ok(BufferedSolenoid {
+                buffer: &self.buffer,
+                channel: channel,
+            })
+        }
+    }
+
+    /// Flush the cached values to CAN.
+    pub fn flush(&self) -> Result<(), CanError> {
+        let data = self.buffer.lock().unwrap();
+        hal_call!(HAL_SetAllSolenoids(self.module, *data as i32)).map_err(From::from)
+    }
+}
+
+impl<'a> BufferedSolenoid<'a> {
+    /// Set the value of the solenoid. This will cache values to the BufferedPcm, which will not
+    /// actually write out values to CAN until flush() is called.
+    pub fn set(&mut self, value: bool) {
+        let mut data = self.buffer.lock().unwrap();
+        if value {
+            *data |= 1 << self.channel;
+        } else {
+            *data &= !(1 << self.channel);
+        }
+    }
+}
